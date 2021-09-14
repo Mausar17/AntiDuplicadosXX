c613c906bdaa228e45742ae186af0d2644c084cf
mausar17 <mausar_17@hotmail.com>
2021-09-14 03:17:37 -0500

    Finished? Seems like it works, just missing abstraction

--- AntiDuplicadosXX/Program.cs
+++ AntiDuplicadosXX/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using OfficeOpenXml;
 using OfficeOpenXml.FormulaParsing.Excel;
 
@@ -18,40 +19,79 @@ namespace AntiDuplicadosXX
             using var package = new ExcelPackage(file);
 
             var worksheet = package.Workbook.Worksheets[0];
-            int lastIndex = 2;
-
+            int lastIndex = worksheet.Dimension.End.Row;
+            List<int> rowsToKeep = new List<int>();
             List<int> rowsToDelete = new List<int>();
             var cellsInSheet = worksheet.Cells;
-            while (true)
+
+            //while (true)
+            //{
+            //    var temp = cellsInSheet["C" + lastIndex];
+            //    if (temp.Value != null)
+            //    {
+            //        lastIndex++;
+            //    }
+            //    else
+            //    {
+            //        lastIndex--;
+            //        break;
+            //    }
+            //} //get index of last row with text
+
+            for (int mainIndex = 2; mainIndex <= lastIndex; mainIndex++)
             {
-                var temp = cellsInSheet["C" + lastIndex];
-                if (temp.Value != null)
+                if (cellsInSheet["C" + mainIndex].Value.Equals(cellsInSheet["C" + (mainIndex + 1)].Value))
                 {
-                    lastIndex++;
+                    var counter = 1;
+                    var auxIndex = mainIndex;
+                    var constIndex = mainIndex;
+                    if (!rowsToKeep.Contains(mainIndex))
+                    {
+                        Console.WriteLine("Good row: " + mainIndex);
+                        rowsToKeep.Add(mainIndex);
+                    }
+
+                    if (cellsInSheet["G" + auxIndex].Value.Equals(cellsInSheet["G" + (auxIndex + 1)].Value))
+                    {
+                        while (cellsInSheet["G" + constIndex].Value.Equals(cellsInSheet["G" + (auxIndex + 1)].Value))
+                        {
+                            counter++;
+                            auxIndex++;
+                        }
+
+                        auxIndex = constIndex;
+                        while (cellsInSheet["G" + constIndex].Value.Equals(cellsInSheet["G" + (auxIndex - 1)].Value))
+                        {
+                            counter++;
+                            auxIndex--;
+                        }
+                        mainIndex += counter;
+                    }
+                    
                 }
                 else
                 {
-                    lastIndex--;
-                    break;
+                    if (!rowsToKeep.Contains(mainIndex))
+                    {
+                        Console.WriteLine("Good row: " + mainIndex);
+                        rowsToKeep.Add(mainIndex);
+                    }
                 }
-            } //get index of last row with text
+                        
+            } //Get good rows
 
-            for (int mainIndex = 2; mainIndex <= lastIndex; mainIndex++)
-                if (cellsInSheet["C" + mainIndex].Value == cellsInSheet["C" + mainIndex + 1])
+            for (int rowNumber = lastIndex; rowNumber >= 2; rowNumber--)
+            {
+                if (!(rowsToKeep.Contains(rowNumber)))
                 {
-                    if (cellsInSheet["G" + mainIndex].Value == cellsInSheet["G" + mainIndex + 1].Value)
-                    {
-
-                    }
-                    var counter = 1;
-                    var auxIndex = mainIndex;
-                    while (cellsInSheet["C" + auxIndex].Value.Equals(cellsInSheet["C" + auxIndex + 1].Value))
-                    {
-                        counter++;
-                        auxIndex++;
-                    }
+                    worksheet.DeleteRow(rowNumber);
                 }
             }
+
+            package.Save();
+            Console.WriteLine("Press anything to end it all.");
+            Console.ReadKey();
+
         }
     }
 }
